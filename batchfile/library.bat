0397c209ca3c6d1a3f8294d512b0a2f2c63f632b
Jerome Reyes <jerome.reyes64@gmail.com>
2025-02-01 13:39:24 +1300

    add

---
+++ batchfile/library.bat
@@ -0,0 +1,8 @@
+pip install requests
+pip install pytest
+pip install json
+pip install boto3
+pip install jsonpath
+pip install pytest-html
+pip install pytest-html-reporter
+pip install jsonschema
